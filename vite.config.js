62c5f065176c9ea6b6fd7382e4394b2f1a8e9d37
Yadin Nou <nouyadin@gmail.com>
2026-07-26 14:58:42 +1000

    update config url

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "https://yadin-nou.github.io/react-portfolio",
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "https://yadin-nou.github.io/react-portfolio",
+// });
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/react-portfolio/" : "/",
+}));
 // export default defineConfig({
 //   plugins: [react()],
 //   base: "/",
